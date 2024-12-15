31a9c0433ed11513510c4b3cb98e337b2a9e3155
yeshapan <yesha.pandya@outlook.com>
2024-12-15 11:53:43 +0530

    initial setup

--- learningdashboard/settings.py
+++ learningdashboard/settings.py
@@ -73,12 +73,21 @@ WSGI_APPLICATION = 'learningdashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'learningdashboard',  # Name of your new database
+        'USER': 'root',
+        'PASSWORD': 'Yesha22',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
